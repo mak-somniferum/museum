13ec0620b2855853b7819539bdfe1439144e9f17
parkhyewon <ettp77@naver.com>
2020-03-16 17:48:45 +0900

    0316

--- js/index.js
+++ js/index.js
@@ -14,4 +14,18 @@ $('.user .btn').click(function(){
 })
 
 
-
+var swiper = new Swiper('.book-swiper', {
+    slidesPerView: 6,
+    slidesPerGroup: 6,
+      loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+      mousewheel: true,
+      keyboard: true,
+  });
