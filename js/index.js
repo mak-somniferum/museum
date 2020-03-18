aba19df57d816ba8b1a0186ac45c36215cd92e11
mak-somniferum <suhjin700@gmail.com>
2020-03-18 18:38:36 +0900

    0318-last-jin

--- js/index.js
+++ js/index.js
@@ -8,11 +8,26 @@ window.onscroll = function(){
     }
 }
 
+
 // 상단 유저 버튼 클릭시 로그인, 회원가입 레이어 fadeToggle
 $('.user .btn').click(function(){
     $('.user .user_bx').fadeToggle()
 })
 
+// 모바일 메뉴 버튼 클릭시 모바일 메뉴 slideToggle
+$('.m_menu_btn').click(function(){
+    $(this).toggleClass('change')
+    $('#m_gnb').slideToggle()
+})
+
+$(window).resize(function(){
+    var w = $(window).width()
+    // window 길이가 990보다 클 때 모바일 메뉴 닫기
+    if(w>990){
+        $('#m_gnb').slideUp()
+        $('.m_menu_btn').removeClass('change')
+    }
+})
 
 var swiper = new Swiper('.book-swiper', {
     slidesPerView: 6,
