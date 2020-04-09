6d51e5f7a652cbd852ba840ee2d663ba0ea9624e
mak-somniferum <suhjin700@gmail.com>
2020-04-09 16:28:29 +0900

    0409-jin

--- js/index.js
+++ js/index.js
@@ -69,43 +69,14 @@ function board(){
     }
 }
 
-// .contest_art 이전 수상작 둘러보기
 
-$('.content button').click(function(){
-    $('.contest_art').fadeToggle().css({'display':'flex'})
-})
-
-//book_slide
-
-$('.next').click(function(){
-    $('.book_wrap').animate({marginLeft:'-200%'},function(){
-        $('.book_wrap .book_slide').first().appendTo('.book_wrap')
-        $('.book_wrap').css({marginLeft:'-100%'})
-    })
-})
-
-$('.prev').click(function(){
-    $('.book_wrap').animate({marginLeft:'0%'},function(){
-        $('.book_wrap .book_slide').last().prependTo('.book_wrap')
-        $('.book_wrap').css({marginLeft:'-100%'})
-    })
-})
-
-//book_slide click 시
-
-$('.book_slide').click(function(){
-    var idx = $(this).index()
-    console.log(idx)
-    $('.book_deta li').slideUp()
-    $('.book_deta li').eq(idx).slideDown().css({'display':'flex'})
-})
 
 //worldArt slide
 
 var mLeft = 0
 var timer = setInterval(move, 20)
 
-$('..worldArt_slide_box').mouseenter(function(){
+$('.worldArt_slide_box').mouseenter(function(){
     clearInterval(timer)
 })
 
@@ -124,20 +95,3 @@ function move(){
     }
 }
 
-
-
-var $grid = $(".grid").isotope({
-    itemSelector: '.grid-item',
-    masonry: {
-      columnWidth: 200
-    }
-  })
-  $(".art_category").on("click", "li", function () {
-      var filterValue = $(this).attr("data-filter")
-    $grid.isotope({ filter: filterValue })
-  })
-
-  $('.art_category li').click(function(){
-      $('.art_category li').removeClass('on')
-      $(this).addClass('on')
-  })
